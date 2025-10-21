484931d4a3982402dc34481fde0ce191ce2854ee
Helena Pedro <mbeua94@gmail.com>
2025-10-21 12:53:06 -0700

    First Commit

---
+++ EEMS/src/main/java/edu/miu/eems/App.java
@@ -0,0 +1,13 @@
+package edu.miu.eems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
